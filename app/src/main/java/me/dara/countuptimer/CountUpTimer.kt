1caca9556bf4ea3a72d330a0c8302e1f779cc5c7
= <sardor.islomov.96@gmail.com>
2018-10-12 07:33:15 +0500

    CustomHandler and CustomerHandlerThread added

--- app/src/main/java/me/dara/countuptimer/CountUpTimer.kt
+++ app/src/main/java/me/dara/countuptimer/CountUpTimer.kt
@@ -1,38 +1,71 @@
 package me.dara.countuptimer
 
-import android.os.Handler
-import android.os.HandlerThread
+import android.os.*
+import java.lang.ref.WeakReference
+
 
 /**
  * @author sardor
  */
 abstract class CountUpTimer(threadType: Int) {
-
   companion object {
     const val MAIN_THREAD = 0
     const val WORKER_THREAD = 1
+    const val MSG = 2
   }
 
-  val mHandler: Handler
-  val mHandlerThread: HandlerThread
+  val mHandler: CustomHandler
+  val mHandlerThread: CustomHandlerThread
   val threadType: Int = threadType
+  var baseTime: Long = SystemClock.currentThreadTimeMillis()
 
   init {
-    mHandler = Handler()
-    mHandlerThread = HandlerThread("WorkerThread")
+    mHandlerThread = CustomHandlerThread("WorkerThread",this)
+    mHandler = CustomHandler(Looper.getMainLooper(),this)
   }
 
   fun start() {
-    val startTime = System.currentTimeMillis()
-    if (threadType == MAIN_THREAD){
-      mHandler.postDelayed({
-
-      }, 50)
-    }else{
-      mHandlerThread.start()
+    baseTime = SystemClock.elapsedRealtime()
+    if (threadType == MAIN_THREAD) {
+      mHandler.sendMessage(mHandler.obtainMessage(MSG))
+    } else {
+      mHandlerThread.sendMessage(mHandlerThread.handler.obtainMessage(MSG))
     }
-
   }
 
+  fun stop() {
+    if (threadType == MAIN_THREAD) {
+      mHandler.removeMessages(MSG)
+    } else {
+      mHandlerThread.handler.removeMessages(MSG)
+    }
+  }
 
+  abstract fun onTick(elapsedTime: Long)
+
+}
+
+class CustomHandler(looper: Looper,
+                    countUpTimer: CountUpTimer) : Handler(looper) {
+  private val mCountUpTimer: WeakReference<CountUpTimer> = WeakReference(countUpTimer)
+
+  override fun handleMessage(msg: Message?) {
+    val baseTime  = mCountUpTimer.get()?.baseTime
+    baseTime?.let {
+      val elapsedTime = SystemClock.currentThreadTimeMillis() - it
+      mCountUpTimer.get()?.onTick(elapsedTime)
+      sendMessageDelayed(obtainMessage(), 1000)
+    }
+  }
+}
+
+open class CustomHandlerThread(name: String,countUpTimer: CountUpTimer) : HandlerThread(name) {
+  val handler: CustomHandler
+  override fun onLooperPrepared() {
+    super.onLooperPrepared()
+    handler = CustomHandler(looper,countUpTimer)
+  }
+  fun sendMessage(message: Message) {
+    handler.sendMessage(message)
+  }
 }
--- app/src/main/java/me/dara/countuptimer/MainActivity.kt
+++ app/src/main/java/me/dara/countuptimer/MainActivity.kt
@@ -2,13 +2,18 @@ package me.dara.countuptimer
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.os.CountDownTimer
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
-
+    val timer = object : CountUpTimer(CountUpTimer.MAIN_THREAD){
+      override fun onTick(elapsedTime: Long) {
+        Toast.makeText(applicationContext,elapsedTime.toString(),Toast.LENGTH_SHORT).show()
+      }
+    }
+    timer.start()
   }
 }
